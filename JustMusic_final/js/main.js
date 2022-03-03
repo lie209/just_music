f844feb621db44fe9e08672082f02d8b994216b1
lie209 <42464199+lie209@users.noreply.github.com>
2022-03-03 17:00:05 +0800

    新版更新

--- JustMusic_final/js/main.js
+++ JustMusic_final/js/main.js
@@ -8,13 +8,16 @@ var app=new Vue({
 		searchResultShow:false,
 		noSearchShow:true,
 		showComment:false,
+		showLyric:false,
 		commentPaneShow:false,
+		lyricPaneShow:false,
 		haveSearched:0,
 		musicQuery:"周杰伦",
 		songsArray:[],
 		musicSrc:"",
 		music_name:"",
 		album_src:"",
+		lyric_src:[],
 		rankList1:[],
 		rankList2:[],
 		rankList3:[],
@@ -22,6 +25,14 @@ var app=new Vue({
 	},
 	mounted:function(){
 		this.indexShow();
+		//获取一言
+		axios.get('https://v1.hitokoto.cn')
+		    .then(({ data }) => {
+		      const hitokoto = document.getElementById('hitokoto_text')
+		      hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
+		      hitokoto.innerText = data.hitokoto
+		    })
+		    .catch(console.error)
 	},
 	methods:
 	{
@@ -34,18 +45,18 @@ var app=new Vue({
 			var that=this;
 			//更新三个排行榜的歌曲数组
 			// 飙升榜
-			axios.get("http://120.79.18.36:3000/playlist/detail?id=19723756").then(function(response)
+			axios.get("http://120.77.38.200:3000/playlist/detail?id=19723756").then(function(response)
 			{
 				console.log(response);
 				that.rankList1=response.data.playlist.tracks;
 			},function(err){});
 			// 新歌榜
-			axios.get("http://120.79.18.36:3000/playlist/detail?id=3779629").then(function(response)
+			axios.get("http://120.77.38.200:3000/playlist/detail?id=3779629").then(function(response)
 			{
 				that.rankList2=response.data.playlist.tracks;
 			},function(err){});
 			// 原创榜
-			axios.get("http://120.79.18.36:3000/playlist/detail?id=2884035").then(function(response)
+			axios.get("http://120.77.38.200:3000/playlist/detail?id=2884035").then(function(response)
 			{
 				that.rankList3=response.data.playlist.tracks;
 			},function(err){});
@@ -59,16 +70,23 @@ var app=new Vue({
 			this.musicListShow=false;
 			this.musicFmShow=false;
 			this.commentPaneShow=false;
+			this.lyricPaneShow=false;
 			this.searchResultShow=true;
 			this.isShow=true;
 			//歌曲排行失去焦点，消除背景底色
 			let oAR=document.getElementById("rankLabel");
 			oAR.style.backgroundColor="transparent";
-			
+			//去除歌曲搜索这一栏的背景颜色
 			let oA=document.getElementById("searchLabel");
 			oA.style.backgroundColor="#DEDEDE";
+			//歌词失去焦点，消除背景颜色
+			let oL=document.getElementById("LyricLabel");
+			oL.style.backgroundColor="transparent";
+			//去除歌曲评论这一栏的背景颜色
+			let oC=document.getElementById("commentLabel");
+			oC.style.backgroundColor="transparent";
 			var that=this;
-			axios.get("http://120.79.18.36:3000/search?keywords= " + this.musicQuery).then(function(response)
+			axios.get("http://120.77.38.200:3000/search?keywords= " + this.musicQuery).then(function(response)
 			{
 				//console.log(response);
 				that.songsArray=response.data.result.songs;
@@ -82,6 +100,7 @@ var app=new Vue({
 			this.musicFmShow=false;
 			this.searchResultShow=false;
 			this.commentPaneShow=false;
+			this.lyricPaneShow=false;
 			//歌曲排行获得焦点，增加背景颜色
 			let oAR=document.getElementById("rankLabel");
 			oAR.style.backgroundColor="#DEDEDE";
@@ -91,6 +110,9 @@ var app=new Vue({
 			//去除歌曲评论这一栏的背景颜色
 			let oC=document.getElementById("commentLabel");
 			oC.style.backgroundColor="transparent";
+			//歌词失去焦点，消除背景颜色
+			let oL=document.getElementById("LyricLabel");
+			oL.style.backgroundColor="transparent";
 		},
 		// fmMusic:function()
 		// {
@@ -112,6 +134,7 @@ var app=new Vue({
 			this.musicFmShow=false;
 			this.searchResultShow=true;
 			this.commentPaneShow=false;
+			this.lyricPaneShow=false;
 			//歌曲排行失去焦点，消除背景底色
 			let oAR=document.getElementById("rankLabel");
 			oAR.style.backgroundColor="transparent";
@@ -121,6 +144,9 @@ var app=new Vue({
 			//去除歌曲评论这一栏的背景颜色
 			let oC=document.getElementById("commentLabel");
 			oC.style.backgroundColor="transparent";
+			//歌词失去焦点，消除背景颜色
+			let oL=document.getElementById("LyricLabel");
+			oL.style.backgroundColor="transparent";
 		},
 		
 		//选中歌曲评论这一栏
@@ -131,6 +157,7 @@ var app=new Vue({
 			this.searchResultShow=false;
 			this.noSearchShow=false;
 			this.commentPaneShow=true;
+			this.lyricPaneShow=false;
 			//歌曲评论获得焦点，设置背景颜色
 			let oC=document.getElementById("commentLabel");
 			oC.style.backgroundColor="#DEDEDE";
@@ -140,6 +167,57 @@ var app=new Vue({
 			//歌曲搜索失去焦点，消除背景底色
 			let oA=document.getElementById("searchLabel");
 			oA.style.backgroundColor="transparent";
+			//歌词失去焦点，消除背景颜色
+			let oL=document.getElementById("LyricLabel");
+			oL.style.backgroundColor="transparent";
+		},
+		//点击歌词按钮
+		lyric:function()
+		{
+			this.musicListShow=false;
+			this.musicFmShow=false;
+			this.searchResultShow=false;
+			this.noSearchShow=false;
+			this.commentPaneShow=false;
+			this.lyricPaneShow=true;
+			//歌词获得焦点，设置背景颜色
+			let oL=document.getElementById("LyricLabel");
+			oL.style.backgroundColor="#DEDEDE";
+			//歌曲失去获得焦点，消除背景颜色
+			let oC=document.getElementById("commentLabel");
+			oC.style.backgroundColor="transparent";
+			//歌曲排行失去焦点，消除背景底色
+			let oAR=document.getElementById("rankLabel");
+			oAR.style.backgroundColor="transparent";
+			//歌曲搜索失去焦点，消除背景底色
+			let oA=document.getElementById("searchLabel");
+			oA.style.backgroundColor="transparent";
+			
+		},
+		
+		split:function(lrc)
+		{//把lrc歌词分割成数组，
+		    let split_1 =lrc.split('\n');
+			let length = split_1.length;
+			for (let i = 0; i < length; i++) 
+			{
+				let lrcArr = split_1[i];
+				split_1[i] = change(lrcArr);
+				function change(str)
+				{
+					let lrc = str.split(']');
+					let timer =lrc[0].replace('[','');
+					let str_music =lrc[1];
+					let time_split =timer.split(':');
+					let s = +time_split[1];
+					let min = +time_split[0];
+					return{
+					time:min*60 + s ,
+					lrc :str_music//分割好到歌词和时间
+					}    
+				}
+			}
+			return split_1
 		},
 		// prePage:function()
 		// {
@@ -150,25 +228,36 @@ var app=new Vue({
 		// 	history.go(+1);
 		// },
 		//点击歌曲名时播放对应的音乐
+		
 		playMusic:function(music_id,album_id,music_name)
 		{
 			var that=this;
 			this.music_name=music_name;
 			this.showComment=true;
+			this.showLyric=true;
 			//获取歌曲src
-			axios.get("http://120.79.18.36:3000/song/url?id=" +music_id).then(function(response)
+			axios.get("http://120.77.38.200:3000/song/url?id=" +music_id).then(function(response)
 			{
 				//console.log(response);
 				that.musicSrc=response.data.data[0].url;
 			},function(err){});
+			//获取歌词src
+			//防止影响，提前清空
+			this.lyric_src=[];
+			axios.get("http://120.77.38.200:3000/lyric?id=" +music_id).then(function(response)
+			{
+				/* console.log(response.data.lrc.lyric); */
+				that.lyric_src=that.split(response.data.lrc.lyric);
+				/* console.log(that.lyric_src[0].lrc); */
+			},function(err){});
 			//获取专辑封面
-			axios.get("http://120.79.18.36:3000/album?id="+album_id).then(function(response)
+			axios.get("http://120.77.38.200:3000/album?id="+album_id).then(function(response)
 			{
 				// console.log(response);
 				that.album_src=response.data.album.picUrl;
 			},function(err){});
 			//获取评论
-			axios.get("http://120.79.18.36:3000/comment/music?id="+music_id).then(function(response)
+			axios.get("http://120.77.38.200:3000/comment/music?id="+music_id).then(function(response)
 			{
 				//console.log(response);
 				that.comments=response.data.hotComments;
@@ -178,21 +267,31 @@ var app=new Vue({
 		playListMusic:function(music_id,music_name)
 		{
 			this.showComment=true;
+			this.showLyric=true;
 			this.music_name=music_name;
 			var that=this;
 			//获取歌曲src
-			axios.get("http://120.79.18.36:3000/song/url?id=" +music_id).then(function(response)
+			axios.get("http://120.77.38.200:3000/song/url?id=" +music_id).then(function(response)
 			{
-				//console.log(response);
+				console.log(response);
 				that.musicSrc=response.data.data[0].url;
 			},function(err){});
+			//获取歌词src
+			//防止影响，提前清空
+			this.lyric_src=[];
+			axios.get("http://120.77.38.200:3000/lyric?id=" +music_id).then(function(response)
+			{
+				/* console.log(response.data.lrc.lyric); */
+				that.lyric_src=that.split(response.data.lrc.lyric);
+				/* console.log(that.lyric_src[0].lrc); */
+			},function(err){});
 			//根据歌曲id获取专辑封面
-			axios.get("http://120.79.18.36:3000/song/detail?ids="+music_id).then(function(response)
+			axios.get("http://120.77.38.200:3000/song/detail?ids="+music_id).then(function(response)
 			{
 				that.album_src=response.data.songs[0].al.picUrl;
 			},function(err){});
 			//获取评论
-			axios.get("http://120.79.18.36:3000/comment/music?id="+music_id).then(function(response)
+			axios.get("http://120.77.38.200:3000/comment/music?id="+music_id).then(function(response)
 			{
 				//console.log(response);
 				that.comments=response.data.hotComments;
